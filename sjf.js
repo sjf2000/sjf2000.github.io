64fdcef7583ee1d6a09decc67776a5d16a2bf14d
sjf2000 <sjfeng2000@163.com>
2014-12-30 09:52:38 +0800

    1

--- sjf.js
+++ sjf.js
@@ -1,21 +1,4 @@
-var divs;
-var top;
-var store_view;
-function main(){
-  divs=new Object();
-  top=document.getElementById("top");
-  top.className="top";
-
-  store_view = document.createElement("div");
-  store_view.id="store_view";
-
-  var product_file_name = "product.txt";
-  var httpRequest =new XMLHttpRequest();
-  httpRequest.open("GET",product_file_name,false);
-  httpRequest.onloadend=processProductString;
-  httpRequest.send(null);
-  httpRequest=null;
-}
-function processProductString(s){
-  window.alert("");
-}
+var m = new Map();
+m.set("a", "1");
+m.set("b", "2");
+window.alert(m.get("b"));
